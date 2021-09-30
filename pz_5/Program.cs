04b67302bc5ac9c186c22ba68bb6394882af18bc
Андрей Чернышев <chernshev97@mail.ru>
2021-09-30 11:25:49 +0500

    test edit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -7,16 +7,28 @@ namespace pz_5
         static void Main(string[] args)
         {
             Console.WriteLine("Задание 1");
-            { 
-             int a;
-                for (a = 20; a <= 90; a += 5)
+            {             
+                for (int a = 20; a <= 90; a += 5)
                 {
                     Console.WriteLine(a);
                 }
             }
             Console.WriteLine("Задание 2");
             {
-
+                string[] a = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", };
+                for (int b = 7; b <= 11; b++) Console.Write(a[b]);
+            }
+            Console.WriteLine("\nЗадание 3");
+            {
+                for (int i = 1; i <= 8; i++)
+                {
+                    for (int k = 1; k <= 7; k++) Console.Write("#");
+                    Console.WriteLine("");
+                }
+            }
+            Console.WriteLine("Задание 4");
+            {
+                for (int u = 0; u <= 100; u++) if (u % 13 == 0) Console.Write(" " + u);
             }
         }
     }
