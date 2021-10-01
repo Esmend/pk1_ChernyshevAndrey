65cf3275756a772e210448821daeac304492a1bb
Андрей Чернышев <chernshev97@mail.ru>
2021-10-01 13:27:58 +0500

    test edit

---
+++ proverka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace proverka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sym1 = 'a';
+            char sym2 = 'y';
+            char result = sym1 == sym2 ? sym1++ : sym2++;
+
+        }
+    }
+}
---
+++ pz_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+            while (b < 10)
+            {
+                b++;                            
+                Console.WriteLine($"{a*b} = {a} * {b}");
+            }                          
+        }
+    }
+}
