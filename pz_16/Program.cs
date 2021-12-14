f200b9f21b21cb31605e042e26756d65ac431e61
Андрей Чернышев <chernshev97@mail.ru>
2021-12-14 21:39:18 +0500

    test

---
+++ pz_16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pz_16
+{
+    class Program
+    {
+        public static int Quarter(int x, int y)
+        {
+            int result = 0;
+
+            if (x > 0)
+            {
+                if (y > 0)
+                    result = 1;
+                else if (y < 0)
+                    result = 4;
+            }
+            else 
+            {
+                if (y > 0)
+                    result = 2;
+                else if (y < 0)
+                    result = 3;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int c = Quarter(x, y);
+            Console.Write("Ваша четверть: ");
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ pz_17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace pz_17
+{
+    class Program
+    {
+        static string Papka(string a)
+        {        
+            
+            
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(a);
+                foreach (string b in dirs)
+                {
+                    Console.WriteLine(b);
+                }
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(a);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Путь к файлу: ");
+            string n = Console.ReadLine();
+            string m = Papka(n);
+            Console.WriteLine(m);
+        }
+    }
+}
